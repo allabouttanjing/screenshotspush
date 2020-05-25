ffee92da4285e6c7ffd6b79dce6519aac187a4fd
morgan <uoozhang@gmail.com>
2020-05-24 21:15:50 -0600

    remove executablePath from launch config

--- src/common/config.ts
+++ src/common/config.ts
@@ -1,8 +1,4 @@
 export const LaunchConfig = {
   args: ['--no-sandbox'],
-  headless: false,
-  executablePath:
-    '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
-  //  "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe",
-  // "/usr/bin/chromium-browser",
+  headless: true,
 };
